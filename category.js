c18b6a5634ef0876033fc30423e1c2e1652b95bf
Alessia Amore <a.amore9393@gmail.com>
2022-02-10 19:32:30 +0100

    add js for category page

---
+++ category.js
@@ -0,0 +1,37 @@
+const url = "https://kea-alt-del.dk/t7/api/products?limit=4";
+
+// const url = "https://kea-alt-del.dk/t7/api/products";
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => handleProductList(data));
+
+function handleProductList(data) {
+  //console.log(data);
+  data.forEach(showProduct);
+}
+
+function showProduct(product) {
+  console.log(product);
+
+  //grab template
+  const template = document.querySelector("template").content;
+
+  //clone it
+  const myClone = template.cloneNode(true);
+
+  //change content
+  myClone.querySelector(".season").textContent = product.season;
+  myClone
+    .querySelector(".alink")
+    .setAttribute("href", `productlist.html?season=${product.season}`);
+  myClone.querySelector(
+    ".alink img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+
+  //grab parent
+  const parent = document.querySelector(".categories");
+
+  //append it
+
+  parent.appendChild(myClone);
+}
